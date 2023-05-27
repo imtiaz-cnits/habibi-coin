ad9e418366e81666b98d178ccc411e744a15f3d7
rahatcnits <rahatcnits@gmail.com>
2023-05-27 09:51:14 +0600

    5th commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
+// parallax moon
+
 gsap.from("#img-1", {
     scrollTrigger : {
         scrub: true
     },
     y: -400,
 })
+
+
+// accordion
+
+const accordion = document.getElementsByClassName("contentBox");
+
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle("active")
+    });
+}
 
