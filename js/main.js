c3e0c5a0823505577e2956bbd86ec098a5f5f101
rahatcnits <rahatcnits@gmail.com>
2023-05-27 11:08:46 +0600

    6th commit

--- js/main.js
+++ js/main.js
@@ -18,3 +18,55 @@ for (i = 0; i < accordion.length; i++) {
     });
 }
 
+
+// chart
+
+const chartData = {
+    labels: ["Supply", "Liquidity", "Marketing", "Cex Listing", "Airdrop"],
+    data: [35, 30, 20, 15, 05],
+};
+
+const myChart = document.querySelector(".my-chart");
+const ul = document.querySelector(".chart-stats .details ul");
+
+new Chart(myChart, {
+    type: "doughnut",
+    data: {
+        lebels: chartData.labels,
+        datasets: [
+            {
+                data: chartData.data,
+                backgroundColor: [
+                    "#ffe600",
+                    "#B6DB71",
+                    "#2EB9DE",
+                    "#FF8E31",
+                    "#9F64D0",
+                ],
+            },
+        ],
+    },
+    options: {
+        borderRadius: 6,
+        elements: {
+            arc: {
+                borderWidth: 0
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    },
+});
+
+const populateUl = () => {
+    chartData.labels.forEach((l, i) => {
+        let li = document.createElement("li");
+        li.innerHTML = `${l} - <span class='percentage'>${chartData.data[i]}%</span>`; 
+        ul.appendChild(li);
+    })
+}
+
+populateUl();
